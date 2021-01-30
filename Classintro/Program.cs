35bbc1b28cbe184f533cabef4cdc7e82b6cf76fa
Yusufbbli34@outlook.com <Yusufbbli34@outlook.com>
2021-01-30 20:09:35 +0300

    Add project files.

---
+++ Classintro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Classintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "yusuf";
+            int yas = 19;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.Egitmen = "eNGIN";
+            kurs1.IzlenmeOrani = 68;
+            kurs1.Kursadi = "C#";
+
+            Kurs kurs2 = new Kurs();
+            kurs2.Egitmen = "Ahmet";
+            kurs2.IzlenmeOrani = 50;
+            kurs2.Kursadi = "python";
+
+            Kurs kurs3 = new Kurs();
+            kurs3.Egitmen = "Mehmet";
+            kurs3.IzlenmeOrani = 80;
+            kurs3.Kursadi = "Java";
+
+            //Console.WriteLine(kurs1.Kursadi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3};
+
+            foreach (Kurs takmaisim in kurslar)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine(takmaisim.Egitmen + " : " + takmaisim.Kursadi);
+                Console.WriteLine(takmaisim.IzlenmeOrani);
+                
+            }
+        }
+    }
+    class Kurs
+    {
+        public string Kursadi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+        
+    }
+}
+foreach (var item in collection)
+{
+
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String kurs1 = "1";
+            String kurs2 = "2";
+            String kurs3 = "3";
+
+            String[] kurslar = new string[] {"1","2","3"};
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+           
+            // foreach dizi temelli yapilari tek tek donmeye yarar
+            foreach (string kurs in kurslar)
+            //kurs kisminda ne yazdigi onemli degil
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        
+        }
+    }
+}
